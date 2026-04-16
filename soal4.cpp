29fdb7ab726cf1ff6fe2c4917239829f4a829a89
ikankeselek <154447514+ikankeselek@users.noreply.github.com>
2026-04-16 23:56:30 +0700

    Update soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -5,16 +5,22 @@ int main() {
     int n;
     cout << "Masukkan n : ";
     cin >> n;
-    if (n < 0 || n > 10000) {
-        cout << "n harus bilangan positif" << endl;
-        return 1;
+    if (n < 1 || n > 10000) {
+        cout << "Gk bisa n nya tu, cak tukar";
     }
-    cout << 'H';
-    for (int i = 0; i < n; i++) cout << 'o';
-    cout << 'r';
-    for (int i = 0; i < n; i++) cout << 'e';
-    for (int i = 0; i < n; i++) cout << '!';
 
-    cout << endl;
+    else {
+        cout << 'H';
+        for (int i = 0; i < n; i++) cout << 'o';
+        cout << 'r';
+        for (int i = 0; i < n; i++) cout << 'e';
+        for (int i = 0; i < n; i++) cout << '!';
+    
+        cout << endl;
+    }
     return 0;
 }
+
+// Nilai n dibatasi, tidak boleh dibawah 1 atau diatas 10000
+// karena "H" dan "r" hanya dicetak sekali, gk perlu perulangan
+// "o", "e", dan "!" dicetak berkali kali, jadi perlu perulangan
