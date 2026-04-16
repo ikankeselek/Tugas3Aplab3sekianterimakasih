2778bca0628575433c85f018cb6831685e21c518
ikankeselek <154447514+ikankeselek@users.noreply.github.com>
2026-04-16 23:46:22 +0700

    Update soal4.cpp

--- soal4.cpp
+++ soal4.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Masukkan n : ";
+    cin >> n;
+    if (n < 0 || n > 10000) {
+        cout << "n harus bilangan positif" << endl;
+        return 1;
+    }
+    cout << 'H';
+    for (int i = 0; i < n; i++) cout << 'o';
+    cout << 'r';
+    for (int i = 0; i < n; i++) cout << 'e';
+    for (int i = 0; i < n; i++) cout << '!';
+
+    cout << endl;
+    return 0;
+}
