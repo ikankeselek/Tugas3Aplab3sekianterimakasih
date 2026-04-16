e4caca032ae03401ce76b46672ee70ebbe09d886
ikankeselek <154447514+ikankeselek@users.noreply.github.com>
2026-04-17 00:14:38 +0700

    Update soal6.cpp

--- soal6.cpp
+++ soal6.cpp
@@ -1,1 +1,20 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int n;
+    cout << "Masukkan ilangan genap: ";
+    cin >> n;
+
+    int jumlah = 0;
+
+    for (int i = 2; i <= n; i += 2) {
+        jumlah += i * i;
+    }
+    cout << jumlah;
+    return 0;
+}
+
+// Dimulai dengan i = 2, kemudian setiap kali sudah melakukan 1 perulangan, maka, nilai i akan bertambah 2
+// Lalu setiap bilangan genap di kuadratkan 
+// loop berhenti saat i > n
